49f1df0cccd705afbd6769fbadbec1ce4e594a01
Nerea <nereaparicio@gmail.com>
2019-02-28 09:46:35 +0100

    starting

---
+++ starter-code/src/components/misc/HeaderCol.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function HeaderCol (){
+
+    return (
+<table className="table container">
+  <thead>
+    <tr>
+      <th><abbr title="Position">Image</abbr></th>
+      <th><abbr title="Name">Name</abbr></th>
+      <th><abbr title="Popularity">Popularity</abbr></th>
+
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      
+    </tr>
+  </tfoot>
+  <tbody>
+    <tr>
+    </tr>
+  </tbody>
+</table>
+    )
+  }
+
